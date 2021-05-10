9cbed8a04a2eeeedf55c9ee826e565ec495e0ab4
Leonardo Castro <80797773+magn3tico@users.noreply.github.com>
2021-05-10 18:25:50 -0300

    Update webcam.js

--- webcam.js
+++ webcam.js
@@ -1,16 +1,6 @@
 const webcamElement = document.getElementById('webcam');
 const webcam = new Webcam(webcamElement, 'user');
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-]).then(start)
-
-Promisse.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri
-])
 
 webcam.start()
   .then(result =>{
@@ -19,15 +9,3 @@ webcam.start()
   .catch(err => {
     console.log(err);
 });
-
-webcam.addEventListener('play', () =>{
-  const canvas = faceapi.createCanvasFromMedia(webcam)document.body(canvas)
-  const displaySize = {width: webcam.width, height: webcam.height}
-  setInterval(async () =>{
-    const detections = await faceapi.detectAllFaces(webcam,
-      new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-  }, 100)
-  console.log(detections)
-  const resizedDetections = faceapi.resizeResult(detections,displaySize)
-  faceapi.draw.drawDetections(canvas, resizedDetections)
-})
